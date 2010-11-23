0e120997f92c48b0d138f8151d9759d7f802c08f
JB <info@airsoft-base.de>
2010-11-23 06:05:12 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '1ba5d8aab9851b7bd8827cee39cff0a665259a4538adb0eab0ff6bd729f6e458b037be20a6b1020b169393200f7e91bbf78ef401b626ad2f69be740cb40fe4fb'
